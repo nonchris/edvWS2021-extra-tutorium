57fffa4f94dc0b35a633287ccd0d14b5c537c522
nonchris <git@chris-ge.de>
2021-01-08 17:17:02 +0100

    :arrow_upper_right: Added recursion example and for loop over a list
    Forgot to included recursion, which I promised in the first comment.
    Also added an example for loop with a list to show that for loops go index by index and not like character per character when they get a list with string

--- src/strings/02-loops.py
+++ src/strings/02-loops.py
@@ -8,7 +8,19 @@ my_sentence = "Python is really good for beginners, trust me!"
 Best option for a simple, fast iteration over a finite number of itemns
 """
 for letter in my_sentence:
-    print(letter)
+    # print(letter)
+    pass  # just here to make this file executable without print statement
+
+"""
+To demonstrate that a for loop does nothing other than iterating trough an object
+and using object[index] until it got all indices covered
+Means that 'arrays' is one index and not 'a', 'r', 'a'...
+This example uses python lists and no numpy arrays!
+Numpy arrays have built-in functionality for iterations so you don't need loops at all!
+"""
+my_list = ["arrays", "have", "this", "as", "i", "n", "d", "e", "x"]
+for letter in my_list:
+    # print(letter)  # this shows that 'letter' is just a descriptive name for that variable
     pass  # just here to make this file executable without print statement
 
 """
@@ -29,6 +41,29 @@ for i in range(0, len(my_sentence)):
     pass
 
 """
+Recursion
+The holy grale of avoiding loops
+Recursion means that a function calls itself until a break condition is met
+"""
+
+
+def print_val(string, index):
+    """
+    This function calls itself with an other index
+    Each time it prints a number, increases the index
+    And calls the same function again
+    It needs the word / sentence as string
+    And an integer as index
+    """
+    if index < len(string):
+        print(string[index])  # printing string at index
+        print_val(string, index + 1)  # calling fucntion with incremented index
+
+
+# staring recursive function
+print_val(my_sentence, 0)
+
+"""
 A better for i loop example
 Yes, you could do this with a while-loop
 But for is more efficient and provides less room for failure
@@ -45,13 +80,13 @@ for i in range(len(my_sentence)):
 Better while-Loop example
 A loop that runs until you write 'Stop, please!'.
 """
-# word = ""
-# while word != "stop, please!":
-#     print("Say something: ")
-#     word = input()
-#     if word.lower() == "stop":
-#         print("Be polite please...")
-#     elif word == "please":
-#         print("Way better, but no.")
-#
-# print("okay, bye")
+word = ""
+while word != "stop, please!":
+    print("Say something: ")
+    word = input()
+    if word.lower() == "stop":
+        print("Be polite please...")
+    elif word == "please":
+        print("Way better, but no.")
+
+print("okay, bye")
