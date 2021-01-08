b13865a223b74659f12ba8dd7b9a13a690bfc2ca
nonchris <git@chris-ge.de>
2021-01-08 01:51:08 +0100

    :new: Added really basic string examples

---
+++ src/strings/00-hello-world.py
@@ -0,0 +1,11 @@
+# hello world!
+
+# defining some words
+word1 = "Hello"
+word2 = "World"
+
+print("Hello World")  # print something to the terminal
+print()
+print(word1, word2)  # python automatically leaves spaces
+print()
+print(word1 + word2)  # strings are added -> no spaces between words
